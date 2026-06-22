e4c0718da290aed12d518cafcd3452b7b75ee7c3
ayushzAIML <theayushchakraborty@gmail.com>
2026-06-22 22:55:25 +0530

    final

--- Backend/server.js
+++ Backend/server.js
@@ -1,7 +1,6 @@
 require("dotenv").config();
 const app = require("./src/app");
 const connectDB = require("./src/config/db");
-const cors = require("cors");
 
 const port = process.env.PORT || 3000;
 
@@ -11,11 +10,5 @@ async function startServer() {
     console.log(`server is running on ${port}`);
   });
 }
-
-app.use(cors({
-  origin: "http://localhost:5173",   // your Vite frontend URL
-  credentials: true,                 // required for cookies/auth headers
-}));
-
 
 startServer();
